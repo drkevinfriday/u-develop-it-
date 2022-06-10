c9c8e3da1cc25a374923bf167385d55cc5e143a2
drkevinfriday <kevinfriday87@gmail,com>
2022-06-09 23:28:48 -0500

    put request

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/api/candidate/:id', (req, res) => {
                 
     const params = [req.params.id];    
     
-    db.query(sql,params, (err, row) => {
+    db.query(sql, params, (err, row) => {
         if (err) {
           res.status(400).json({ error: err.message });
           return;
@@ -73,6 +73,36 @@ app.get('/api/candidate/:id', (req, res) => {
         });
       });      
     });
+
+app.get('/api/parties',(req,res)=>{
+    const sql =`SELECT * FROM parties`;
+    
+    db.query(sql,(err,rows)=>{
+        if(err){
+            res.status(500).json({error: err.message});
+            return
+        }
+        res.json({
+            message:'success',
+            data: rows
+        })
+    })
+})
+app.get('/api/party/:id',(req,res)=>{
+    const sql =`SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id]
+    
+    db.query(sql,params,(err,rows)=>{
+        if(err){
+            res.status(400).json({error: err.message});
+            return
+        }
+        res.json({
+            message:'success',
+            data: rows
+        })
+    })
+})
 
 //     // Get a single candidate
 // app.get('/api/candidate/:id', (req, res) => {
@@ -99,7 +129,28 @@ app.get('/api/candidate/:id', (req, res) => {
 
      db.query(sql,params, (err,result)=>{
          if (err){
-             res.statusMessage(400).json({error: res.message});
+             res.status(400).json({error: res.message});
+         } else if(!result.affectedRows){
+             res.json({
+                 message: 'Candidate not found'
+             });                    
+         } else {
+             res.json({
+                 message: 'deleted',
+                 changes: result.affectedRows,
+                 id: req.params.id
+             })
+         }
+     })
+ })
+
+ app.delete('/api/party/:id',(req, res)=>{
+     const sql = `DELETE FROM  parties WHERE id= ?`;
+     const params = [req.params.id];
+
+     db.query(sql,params, (err,result)=>{
+         if (err){
+             res.status(400).json({error: res.message});
          } else if(!result.affectedRows){
              res.json({
                  message: 'Candidate not found'
@@ -147,7 +198,36 @@ app.get('/api/candidate/:id', (req, res) => {
      })
  })
 
+//update a candidate's party
+app.put('/api/candidate/:id',(req,res)=>{
+    const errors = inputCheck(req.body, 'party_id');
 
+        if (errors) {
+            res.status(400).json({ error: errors });
+            return;
+        }
+    const sql = `UPDATE candidates SET party_id = ?  
+                 WHERE id = ?`
+    const params = [req.body.party_id, req.params.id]
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+          // check if a record was found
+        } else if (!result.affectedRows) {
+          res.json({
+            message: 'Candidate not found'
+          });
+        } else {
+          res.json({
+            message: 'success',
+            data: req.body,
+            changes: result.affectedRows
+          });
+        }
+      });
+
+})
 
 
 
