c9c9f30ced3fc0aa6c6d3adf629856488950d9bd
drkevinfriday <kevinfriday87@gmail,com>
2022-06-09 21:14:07 -0500

    join statement addition to get request

--- server.js
+++ server.js
@@ -30,7 +30,11 @@ const db = mysql.createConnection(
 // this line displays all the current data from candidates database
 //  gets all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`; 
     
     
     db.query(sql, (err, rows) => {
@@ -48,7 +52,13 @@ app.get('/api/candidates', (req, res) => {
 
 //  get a single candidates 
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `Select * From candidates where id =?`;
+    const sql = `Select candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id
+                where candidates.id =?`;
+                
     const params = [req.params.id];    
     
     db.query(sql,params, (err, row) => {
@@ -107,66 +117,37 @@ app.get('/api/candidate/:id', (req, res) => {
 
  
 
-// // create a candidate
-//  app.post('/api/candidate',({ body }, res)=>{
-//      const errors = inputCheck(
-//          body, 
-//          'first_name',
-//          'last_name',
-//          'industry_connected'
-//          );
-//     if(errors){
-//         res.status(400).json({error: errors});
-//         return;
-//     }
+// create a candidate
+ app.post('/api/candidate',({ body }, res)=>{
+     const errors = inputCheck(
+         body, 
+         'first_name',
+         'last_name',
+         'industry_connected'
+         );
+    if(errors){
+        res.status(400).json({error: errors});
+        return;
+    }
 
         
-//     const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-//     VALUES(?,?,?)`;
-//     const params = [body.first_name, body.last_name, body.industry_connected];
-
-//     db.query(sql, params, (err,result)=>{
-//         if (err){
-//             res.statusMessage(400).json({error: err.message});
-//             return;  
-//         }
-//         res.json({
-//             message: 'success',
-//             data: body
-//         })       
-//      })
-//  })
-
-
-
- // Create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(
-      body,
-      'first_name',
-      'last_name',
-      'industry_connected'
-    );
-    if (errors) {
-      res.status(400).json({ error: errors });
-      return;
-    }
-  
     const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-      VALUES (?,?,?)`;
+    VALUES(?,?,?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
-  
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
-    });
-  });
+
+    db.query(sql, params, (err,result)=>{
+        if (err){
+            res.statusMessage(400).json({error: err.message});
+            return;  
+        }
+        res.json({
+            message: 'success',
+            data: body
+        })       
+     })
+ })
+
+
 
 
 
